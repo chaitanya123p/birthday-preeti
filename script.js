5bfca7027c871d77054b39ac7b38ae2f77c610d4
chaitanya123p <chpetkar@gmail.com>
2026-01-12 20:42:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+function showMessage() {
+    alert("You are amazing just the way you are 💖 Happy Birthday!");
+}
